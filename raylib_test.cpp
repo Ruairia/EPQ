a823da5509e7a3ee8e134ccb81348da128935428
Ruairi <ruairianthony@outlook.com>
2025-07-31 17:30:28 +0100

    Tested raylib's functionality

---
+++ raylib_test.cpp
@@ -0,0 +1,24 @@
+//
+// Created by ruairi on 31/07/2025.
+//
+
+#include "raylib_test.h"
+
+#include "raylib.h"
+
+int main() {
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    InitWindow(screenWidth, screenHeight, "Raycasting Engine");
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("Raylib is working!", 200, 280, 20, RAYWHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
---
+++ raylib_test.h
@@ -0,0 +1,8 @@
+//
+// Created by ruairi on 31/07/2025.
+//
+
+#ifndef RAYLIB_TEST_H
+#define RAYLIB_TEST_H
+
+#endif //RAYLIB_TEST_H
