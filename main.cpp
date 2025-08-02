99b18782b86041c0353fd4bfc4b0d7b7473b8d3b
Ruairi <ruairianthony@outlook.com>
2025-08-02 13:57:47 +0100

    Defined constants and initialised important vector variables.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,38 @@
 #include "Vector2D.h"
 #include <cmath>
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 600
 using namespace raycaster;
+
+int main(){
+    const int map[][] = {
+        {1,1,1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,1,1,0,1},
+        {1,0,0,0,0,0,0,1,0,1},
+        {1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,2,2,2,0,0,1},
+        {1,0,0,0,2,0,0,0,0,1},
+        {1,0,0,0,2,2,0,0,0,1},
+        {1,0,0,0,2,0,0,0,0,1},
+        {1,0,0,0,2,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1,1}
+    };
+    int targetFPS=60;
+    Vector2D playerPosition {1.4,2.3};
+    Vector2D playerDirection {1,0};
+    Vector2D cameraPlane {0,-2};
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
